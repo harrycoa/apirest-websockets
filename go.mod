b038c766f5957733d2ae0dfc3f853f9ea6c29a09
Harry Coa <rodol_2000@hotmail.com>
2022-07-03 11:46:15 -0500

    feat: primer endpoint en servidor

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 )
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/harrycoa/apirest-websockets.git/handlers"
+	"github.com/harrycoa/apirest-websockets.git/server"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// cargar variables de entorno
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	// crear servidor
+	s, err := server.NewServer(context.Background(), &server.Config{
+		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
+		DatabaseURL: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	// Primer endpoint
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+
+}
