da1f081a5d548d0042b5a831290344ee2b89c323
Harry Coa <rodol_2000@hotmail.com>
2022-07-05 17:41:31 -0500

    feat: se implementa la clase postgres para el insert de usuarios y el listado

---
+++ repository/user.go
@@ -0,0 +1,40 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/harrycoa/apirest-websockets.git/models"
+)
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id int64) (*models.User, error)
+	Close() error
+}
+
+var implementation UserRepository
+
+func SetRepository(repository UserRepository) {
+	implementation = repository
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id int64) (*models.User, error) {
+	return implementation.GetUserById(ctx, id)
+}
+
+func Close() error {
+	return implementation.Close()
+}
+
+/*
+************* Abstracciones *************
+Handler - GetUserByIdPostgresql ...
+es una clase concreta // rompe los principios SOLID
+
+implementacion demasiada concreta y cuando se cambie de base de datos tendra problemas, es por ello que se usa
+las abstracciónes (interfaces y sus implementaciones) eso es el patron repository
+*/
